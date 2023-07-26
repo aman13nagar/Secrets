d254fc3783ac5fab7a9600a42394c9d8f4accfb2
aman13nagar <122695931+aman13nagar@users.noreply.github.com>
2023-07-26 08:27:53 +0530

    Second commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://aman13nagar:Aaman@cluster0.zecfzcz.mongodb.net/userDB",{useNewUrlParser:true});
 //mongoose.set("useCreateIndex",true);
 const userSchema= new mongoose.Schema({
     email:String,
